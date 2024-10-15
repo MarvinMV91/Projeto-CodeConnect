214b877ee3e070dbb9e6e38ae6e406c15ce1349a
Marvin Milbradt <marvinmv@icloud.com>
2024-10-15 15:24:48 -0300

    Envio do formulario

--- scripts.js
+++ scripts.js
@@ -83,19 +83,8 @@ inputTags.addEventListener("keypress", async (evento) => {
 
 const botaoPublicar = document.querySelector(".botao-publicar");
 
-botaoPublicar.addEventListener("click", async (evento) => {
-    evento.preventDefault();
-    
-    const nomeDoProjeto = document.getElementById("nome").value;
-    const descricaoDoProjeto = document.getElementById("descricao").value;
-    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
 
-    console.log(nomeDoProjeto);
-    console.log(descricaoDoProjeto);
-    console.log(tagsProjeto);
-});
-
-async function publicarProjeto(nomeDoProjeto, descricaoProjeto, tagsProjeto) {
+async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             const deuCerto = Math.random() > 0.5;
@@ -107,3 +96,20 @@ async function publicarProjeto(nomeDoProjeto, descricaoProjeto, tagsProjeto) {
         }, 2000)
     })
 }
+
+botaoPublicar.addEventListener("click", async (evento) => {
+    evento.preventDefault();
+    
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+    
+    try {
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado);
+        alert("Deu certo!");
+    } catch (error) {
+        console.log("Deu errado: ", error);
+        alert("Deu errado")
+    }
+});
